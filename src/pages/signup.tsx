d70a1539a74fff9e120ebe9636e32ea44ddb0a79
ayaankhan12-beep <muhammadayaanmuhammadshoaib@gmail.com>
2026-03-31 00:57:31 +0500

    ggg

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -28,7 +28,7 @@ export default  function Signup() {
         setloader(true)
 
         try {
-            const res = await axios.post( `${import.meta.env.VITE_FETCH_LINK}/signUp` ,
+            const res = await axios.post( `${import.meta.env.VITE_FETCH_LINK}/signup` ,
                 {username , email , password},
                 {withCredentials: true}
             )
